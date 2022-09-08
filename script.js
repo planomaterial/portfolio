30ccf46314748dd1c92f9a90bb64ea1b454f6c60
planomaterial <112432109+planomaterial@users.noreply.github.com>
2022-09-08 10:08:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let sections = document.querySelectorAll('.animate');
-function activeDisplay() {
-    sections.forEach(element => {
-        element.classList.add('active');
-    });
+window.onload = function() {
+    let animateElements = document.querySelectorAll('.js-animate');
+    function ActiveDisplay() {
+        this.show = function() {
+            animateElements.forEach(e => {
+                e.classList.add('activeAnimate');
+            });
+        };
+    };
+    let showElements = new ActiveDisplay();
+    showElements.show();
 };
-window.addEventListener('load', activeDisplay);
